9bbe30640673bc19bb4adb3462898c62202e0f77
Kamil Rykowski <kamil.rykowski@stxnext.pl>
2018-06-21 17:52:20 +0200

    Fancy variation about card title

--- lib/components/tutorial_item.dart
+++ lib/components/tutorial_item.dart
@@ -12,22 +12,44 @@ class TutorialItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(bottom: 8.0),
+      child: Stack(
+        overflow: Overflow.visible,
+        children: <Widget>[
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                padding: EdgeInsets.only(top: 24.0),
+                child: Text(description, textAlign: TextAlign.center),
+              ),
+            ),
+          ),
+          Positioned(
+            top: -24.0,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              width: 62.0,
+              height: 62.0,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Theme.of(context).cardColor,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 12.0),
                 child: Text(
                   title,
-                  style: TextStyle(fontSize: 24.0),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).primaryColor,
+                  ),
                 ),
               ),
-              Text(description, textAlign: TextAlign.center),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
