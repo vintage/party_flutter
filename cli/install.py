4ab15b952da91e90b41e562c516ef53d9dd0e35e
Kamil Rykowski <kamil.rykowski@stxnext.pl>
2018-11-05 20:07:57 +0100

    Cleanup script for categories json files

---
+++ cli/install.py
@@ -0,0 +1,61 @@
+import os
+import shutil
+from subprocess import call
+import json
+
+import click
+
+LANGUAGES = ['en', 'pl']
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def clean():
+    for language in LANGUAGES:
+        clean_category(language)
+
+
+def clean_category(language):
+    print('---> Cleaning: {}'.format(language))
+
+    categories_path = '../assets/data/categories_{}.json'.format(language)
+
+    with open(categories_path, 'r') as f:
+        categories = json.load(f)
+
+    for category in categories:
+        unique_questions = sorted(set(category['questions']))
+        unique_questions = [q[0].upper() + q[1:] for q in unique_questions]
+
+        prevent_semi_duplicates = []
+        for question in unique_questions:
+            question = question.lower()
+            question = "".join(question.split())
+
+            prevent_semi_duplicates.append(question)
+
+        if len(set(prevent_semi_duplicates)) != len(unique_questions):
+            raise Exception('Semi duplicates in category {}'.format(category['name']))
+
+        category['questions'] = unique_questions
+
+    with open(categories_path, 'w') as f:
+        f.write(json.dumps(categories, indent=2, sort_keys=True, ensure_ascii=False))
+
+    # Sort categories by number of questions
+    categories = sorted(categories, key=lambda k: len(k['questions']), reverse=True)
+
+    for category in categories:
+        print('{}: {}'.format(category['id'], len(category['questions'])))
+
+    questions_count = sum(len(c['questions']) for c in categories)
+
+    print('---> Questions count: {}'.format(questions_count))
+
+
+if __name__ == '__main__':
+    cli()
--- lib/models/category.dart
+++ lib/models/category.dart
@@ -1,29 +1,10 @@
 import 'package:zgadula/models/question.dart';
 
-enum CategoryModeType { show, talk, draw, sing, humming }
-
-CategoryModeType nameToType(name) {
-  switch (name) {
-    case 'show':
-      return CategoryModeType.show;
-    case 'draw':
-      return CategoryModeType.draw;
-    case 'sing':
-      return CategoryModeType.sing;
-    case 'humming':
-      return CategoryModeType.humming;
-  }
-
-  return CategoryModeType.talk;
-}
-
 class Category {
   final String id;
   final String name;
   final String image;
   final String description;
-  final bool isFree;
-  final List<CategoryModeType> modes;
   List<Question> questions;
 
   Category(
@@ -31,8 +12,6 @@ class Category {
     this.name,
     this.image,
     this.description,
-    this.isFree,
-    this.modes,
     this.questions,
   );
 
@@ -41,9 +20,6 @@ class Category {
         name = json['name'],
         image = json['image'],
         description = json['description'],
-        isFree = json['isFree'],
-        modes =
-            List.from((json['modes'] ?? []).map((mode) => nameToType(mode))),
         questions = List.from(json['questions'].map((name) => Question(name)));
 
   String getImagePath() {
