d833b82ad40322b8365a86c9efc66660c1a49b6d
Kamil Rykowski <kamil.rykowski@stxnext.pl>
2019-02-15 20:48:55 +0100

    Migrate to AndroidX

--- lib/services/vibration.dart
+++ lib/services/vibration.dart
@@ -1,13 +1,13 @@
-import 'package:vibrate/vibrate.dart';
+import 'package:vibration/vibration.dart';
 import 'package:flutter/widgets.dart';
 import 'package:zgadula/store/settings.dart';
 
 class VibrationService {
   static vibrate(BuildContext context) async {
-    bool canVibrate = await Vibrate.canVibrate && SettingsModel.of(context).isVibrationEnabled;
+    bool canVibrate = await Vibration.hasVibrator() && SettingsModel.of(context).isVibrationEnabled;
 
     if (canVibrate) {
-      Vibrate.vibrate();
+      Vibration.vibrate();
     }
   }
 }
